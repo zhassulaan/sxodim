b5424e353d8398f2b5bf0becf856eb3437b9c1e1
Zhassulan <190103383@stu.sdu.edu.kz>
2023-04-20 10:08:02 +0600

    correction on frontend

---
+++ frontend/src/components/Button.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+function Button() {
+	return (
+		<Wrapper>
+			<button id="button">
+				Показать еще
+			</button>
+		</Wrapper>
+	);
+}
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	padding: 0px 16px;
+
+	button {
+		display: flex;
+    justify-content: center;
+    align-items: center;
+		min-width: 192px;
+    min-height: 48px;
+    line-height: 20px;
+    font-size: 14px;
+    background: rgb(255, 255, 255);
+    color: rgb(236, 102, 82);
+    border: 1px solid rgb(236, 102, 82);
+    border-radius: 4px;
+	}
+`;
+
+export default Button;
--- frontend/src/screens/Home/components/ImpressionCard.js
+++ frontend/src/screens/Home/components/ImpressionCard.js
@@ -7,7 +7,7 @@ function ImpressionCard({ event }) {
 	return (
 		<Wrapper className="impression-card">
 			<div className="impression-card-image">
-				<a href={ `/event/${event._id}` }>
+				<a href={ `/event/${event.id}` }>
 					<img src={ event.image } alt="Фестиваль Sxodim Fest" className="impression-card-background_image"/>
 				</a>
 				<div className="impression-card-badges">
@@ -16,7 +16,7 @@ function ImpressionCard({ event }) {
 							<img src={ afisha } alt="afisha icon" />
 						</div>
 					</div>
-					<a href={ `/${event.category_en}/${event._id}/tickets` } className="impression-card-badge badge">
+					<a href={ `/${event.category_en}/${event.id}/tickets` } className="impression-card-badge badge">
 						<div className="impression-icon">
 							<img src={ tickets } alt="tickets icon" />
 						</div>
